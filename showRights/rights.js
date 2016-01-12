befa3aac10f7205e9f6ea8cf4595b7aef5f4efad
Julia <y.luchaninova@gmail.com>
2016-01-12 17:21:32 +0100

    изменен rights.js

--- showRights/rights.js
+++ showRights/rights.js
@@ -1,25 +1,39 @@
 /**
  * Created by Julia on 11.01.2016.
  */
-var CHANGE_RIGHTS = 1;//константы для хранения прав
-var WRITE_ART = 2;
-var READ_ART = 4;
+var DELETE = 1;// 001константы для хранения прав
+var WRITE = 2;//010
+var READ = 4;//100
+var guest = READ;
+var moderator = guest | WRITE;
+var admin = moderator | DELETE;
+var accessRights={
+    guest : guest,
+    moderator : moderator,
+    admin : admin
+};
 var key = 1220461917; //произвольный ключ для шифрования
 
 window.onload = function () {
     if (localStorage) {
         var savedUser = JSON.parse(localStorage.getItem('user'));//т.к. объект хранился в localstorage в виде строки, возвращаем вид объекта
-        if(savedUser){
+        if (savedUser) {
             checkPassword(savedUser);//проверяем, есть ли уже сохраненныю юзер
         }
         else {
             document.getElementById('form').addEventListener('submit', function () {
                 var login = document.getElementById('login').value;//сохр в значения переменных данные, введенные в форму
                 var password = document.getElementById('password').value;
-                var role = this.elements.role.value;
-                if(login == ''||password == ''||role ==''){//проверяем все ли строки заполнены
+                //var role = this.elements.role.value;
+                var roles = document.getElementsByName('role');
+                for (var i = 0; i < roles.length; i++) {
+                    if (roles[i].checked) {
+                        var role = (roles[i].value);
+                    }
+                }
+                if (login == '' || password == '' || role == '') {//проверяем все ли строки заполнены
                     alert('вы не все ввели');
-                    return false;//для того чтобы функция дальше не выполнялась
+                    return;//для того чтобы функция дальше не выполнялась
                 }
 
                 var criptedPassword = cript(password, key);//вызываем ф. шифрования пароля
@@ -36,14 +50,22 @@ window.onload = function () {
 };
 
 function cript(password, key) {
-    return password ^ key
+    if (typeof (password) == "string") {
+        return (password.charCodeAt(password.length - 1)) ^ key;
+
+    }
+    else {
+        return password ^ key
+    }
 }
 
-
 function checkPassword(savedUser) {//проверка пароля
-    var checkPass = prompt('введите свой пароль еще раз', '');
-var uncriptedPass = ((savedUser.password)^key);
-    if (checkPass != 0) {//если пользователь ввел пароль
+    var checkPass = cript(prompt('введите свой пароль еще раз', ''), key);
+    var uncriptedPass = (cript(savedUser.password, key) ^ key);
+    if (checkPass == key) {//если пользователь не ввел пароль
+        alert('вы не ввели пароль');
+    }
+    else {
         if (checkPass == uncriptedPass) {
             alert('вы верно ввели пароль');
             showRights(savedUser);//если пароли совпадают - показываем права
@@ -53,21 +75,23 @@ var uncriptedPass = ((savedUser.password)^key);
         }
     }
 }
-function showRights(savedUser){//показываем права юзеру
+function showRights(savedUser) {//показываем права юзеру
     var list = document.getElementById('list');
     var li = document.createElement('LI');
-    if(savedUser.role == CHANGE_RIGHTS){
-        li.innerHTML = 'Вы можете читать и изменять записи, менять пользователей';
+    var check = DELETE | WRITE | READ;
+    if (( accessRights[savedUser.role] & check) == 4) {
+        li.innerHTML = 'Вы не можете' + ' ';
+        list.appendChild(li);
+    }
+    if ((accessRights[savedUser.role] & check) == 6) {
+        li.innerHTML = 'Вы можете' + ' ';
         list.appendChild(li);//добавляем элемент в список
     }
-    else if(savedUser.role == WRITE_ART){
-        li.innerHTML = 'Вы можете читать и изменять записи';
-        list.appendChild(li);
+    if ((accessRights[savedUser.role]& check) == 7) {
+        li.innerHTML = 'Вы можете' + ' ';
+        list.appendChild(li);//добавляем элемент в список
     }
-    else if(savedUser.role == READ_ART){
-        li.innerHTML = 'Вы можете читать записи';
-        list.appendChild(li);
-    }
+
 }
 
 

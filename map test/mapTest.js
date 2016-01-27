c36b2c31c74f4b682b1dce14b25d65506563d3bd
Julia <y.luchaninova@gmail.com>
2016-01-27 13:31:29 +0100

    добавила возможность выбирать коллбеки для тестов + визуализировала коллбеки в html

--- map test/mapTest.js
+++ map test/mapTest.js
@@ -1,38 +1,43 @@
 var testGenerator = {
-        inputArray: [2, 4, null, 0, , 1, '', -6, undefined, [1, 4]],
-        changeArrayFirst: function (a) {
-            return a * 4;
-        },
-        changeArraySecond: function (a) {
-            return a - 2;
-        },
-        changeArrayThird: function (a) {
-            return !a;
-        },
-        changeArrayFourth: function (a) {
-        },
-        testFuncArr: function () {
-            return [testGenerator.changeArrayFirst, testGenerator.changeArraySecond, testGenerator.changeArrayThird, testGenerator.changeArrayFourth];
-        },
-        getExpected: function (mapCallback) {
-            return testGenerator.inputArray.map(mapCallback)
-        },
-        makeTest: function (expected, mapCallback) {
-            it('changes input array and result is : ' + expected, function () {
-                assert.deepEqual(myMap(testGenerator.inputArray, mapCallback), expected)
-            })
-        },
-        makeDescribeBlock: function () {
-            var arrayFunc = testGenerator.testFuncArr();
-            for (var i = 0; i < arrayFunc.length; i++) {
-                describe('changes input array', function () {
+    inputArray: [2, 4, null, 0, , 1, '', -6, undefined, [1, 4]],
+    multuplyFunc: function (a) {
+        return a * 4;
+    },
+    subtractFunc: function (a) {
+        return a - 2;
+    },
+    denyFunc: function (a) {
+        return !a;
+    },
+    noFunc: function (a) {
+    },
+    testFuncArr: function () {
+        return [testGenerator.multuplyFunc, testGenerator.subtractFunc, testGenerator.denyFunc, testGenerator.noFunc];
+    },
+    getExpected: function (mapCallback) {
+        return testGenerator.inputArray.map(mapCallback)
+    },
+    makeTest: function (expected, mapCallback) {
+        it('changes input array and result is : ' + expected, function () {
+            assert.deepEqual(myMap(testGenerator.inputArray, mapCallback), expected)
+        })
+    },
+    makeDescribeBlock: function (name) {
+        var arrayFunc = testGenerator.testFuncArr();
+        for (var i = 0; i < arrayFunc.length; i++) {
+            if (name == arrayFunc[i]) {
+                describe('changes input array by ' + arrayFunc[i], function () {
                     testGenerator.makeTest(testGenerator.getExpected(arrayFunc[i]), arrayFunc[i]);
                 })
             }
         }
-    };
+    }
+};
+testGenerator.makeDescribeBlock(testGenerator.multuplyFunc);
+testGenerator.makeDescribeBlock(testGenerator.subtractFunc);
+testGenerator.makeDescribeBlock(testGenerator.denyFunc);
+testGenerator.makeDescribeBlock(testGenerator.noFunc);
 
-testGenerator.makeDescribeBlock();
 
 
 //
